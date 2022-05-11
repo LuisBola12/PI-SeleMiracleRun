b676260310b9244a8c917d26f805c0fcff84e6ba
Javier Molina Herrera <javmoli045@gmail.com>
2022-05-11 16:59:05 -0600

    agregado un menú para agregar ver los beneficios

---
+++ FrontEnd/src/Components/CrudBenefits/CrudBenefits.js
@@ -0,0 +1,124 @@
+import {
+    Table,
+    Container,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    FormGroup,
+    ModalFooter,
+  } from "reactstrap";
+
+import { useState } from "react";
+import '../../App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const database = [
+  { id: 1, name: "Gym", actualCost: 20000 },
+  { id: 2, name: "Transporte", actualCost: 300000 },
+  { id: 3, name: "Fisioterapia", actualCost: 40000 },
+];
+
+export const CrudBenefits = () => {
+    const [data, setData] = useState(database);
+    const [viewModal, setViewModal] = useState(false);
+    const [name,setName] = useState('');
+    const [cost,setCost] = useState(0);
+    return (
+        <>
+      <Container className="content-container">
+        <br />
+        <button className=" button create-button" onClick={() => setViewModal(true)}>
+          {" "}
+          Create New Benefit
+        </button>
+        <br />
+        <Table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Benefit</th>
+              <th>Actual Cost</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((e) => (
+              <tr key={e.id}>
+                <td>{e.id}</td>
+                <td>{e.name}</td>
+                <td>{e.actualCost}</td>
+                <td>
+                  <button  className=" button"> Edit </button>
+                </td>
+                <td>
+                  <button className=" button cancel-button" > Delete </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+      <Modal isOpen={viewModal}>
+        <ModalHeader>
+          <div>
+            <h3>Insert New Benefit</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <label>Id:</label>
+            <input
+              className="form-control"
+              readOnly
+              type="text"
+              value={data.length + 1}
+            ></input>
+          </FormGroup>
+
+          <FormGroup>
+            <label>Name:</label>
+            <input
+              className="form-control"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </FormGroup>
+
+          <FormGroup>
+            <label>Cost:</label>
+            <input
+              className="form-control"
+              type="number"
+              value={cost}
+              onChange={(e) => setCost(e.target.value)}
+            ></input>
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <button 
+            className="button create-button"
+            onClick={() => {
+              const newData = {
+                id: data.length + 1,
+                name: name,
+                cost: cost,
+              };
+              setData([...data, newData]);
+              setViewModal(false);
+              setName("");
+              setCost("");
+            }}
+          >
+            Insert
+          </button>
+          <button className="button cancel-button" onClick={() => setViewModal(false)}>
+            Cancel
+          </button>
+        </ModalFooter>
+      </Modal>
+    </>
+    )
+
+};
--- FrontEnd/src/Pages/benefits.js
+++ FrontEnd/src/Pages/benefits.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../App.css'
+import { CrudBenefits } from '../Components/CrudBenefits/CrudBenefits';
 
 const  Benefits = () => {
   return (
     <div className = 'page-content'>
-      <h1>Lista de beneficios</h1>
+      <CrudBenefits/>
     </div>
   );
 };
