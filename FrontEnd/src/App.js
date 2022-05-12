dbf7e67423d8606a50f34d93b79daee4f196d931
Javier Molina Herrera <javmoli045@gmail.com>
2022-05-11 19:09:21 -0600

    Corregido un error que hacía que la homepage no se mostrara la primera vez al entrar a la págnina

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
+        <Route path= "/" element={<Home/>} />
         <Route path= "home" element={<Home/>} />
         <Route path= "benefits" element={<Benefits/>} />
         <Route path= "employees" element={<Employees/>} />
