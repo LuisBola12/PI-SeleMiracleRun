8f3618cfed8ecf05a6aff9e2eaf0977f01c36a95
Carlos <carlos.ace27@gmail.com>
2022-05-18 19:27:52 -0600

    Se cambia App.js para incorporar el uso de contextos

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,23 +1,17 @@
 import React, { createContext, useState } from 'react';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/home';
 import Benefits from './Pages/benefits';
 import Employees from './Pages/employees';
 import VolDeductions from './Pages/volDeductions';
 import Contracts from './Pages/contracts';
-import Projects from './Pages/payrollProjects/Projects';
-import CrudProjects from './Components/CrudProjects/CrudProjects';
 import ProjectContext from './Contexts/ProjectContext';
-
+import SelectProject from './Pages/payrollProjects/SelectProject';
 
 
 function App() {
-  const [activeProject, setActiveProject] = useState('Proyecto desdeAoo');
-  const changeActiveProject = (newActiveProject) => {
-    setActiveProject(newActiveProject);
-  };
+  const [activeProject, setActiveProject] = useState('Proyecto sin asignar');
 
   return (
     <ProjectContext.Provider value={{ activeProject, setActiveProject }}>
@@ -30,7 +24,7 @@ function App() {
           <Route path="employees" element={<Employees />} />
           <Route path="volDeductions" element={<VolDeductions />} />
           <Route path="contracts" element={<Contracts />} />
-          <Route path="projectAdmin" element={<CrudProjects />} />
+          <Route path="projectAdmin" element={<SelectProject />} />
         </Routes>
       </Router>
     </ProjectContext.Provider>
