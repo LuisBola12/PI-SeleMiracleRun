574d36fa5c3e3397becfc8817f077cd8d15f8524
Carlos <carlos.ace27@gmail.com>
2022-05-20 12:23:37 -0600

    Se programa el backend para mostrar los proyectos de un determinado empleador

--- BackEnd/src/routes/seleMiracle.routes.js
+++ BackEnd/src/routes/seleMiracle.routes.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
-import { createNewUser, getUsers, getUserByEmail,getPeriodos} from "../controllers/users.controller";
-import {getEmployees} from '../controllers/employees.contoller'
-
+import { createNewUser, getUsers, getUserByEmail, getPeriodos } from "../controllers/users.controller";
+import { getEmployees } from '../controllers/employees.contoller';
+import { getProjectsByEmail } from "../controllers/projects.controller";
 
 const router = Router();
-router.get('/users',getUsers);
-router.post('/users',createNewUser);
-router.get('/users/:Email',getUserByEmail);
+router.get('/users', getUsers);
+router.post('/users', createNewUser);
+router.get('/users/:Email', getUserByEmail);
 // router.get('/periodos',getPeriodos);
 // router.delete('/users',getUsers);
 // router.put('/users',getUsers);
-router.get('/employees',getEmployees);
+router.get('/employees', getEmployees);
+
+router.get('/projects/:Email', getProjectsByEmail);
+// router.post('/projects:User', createNewProjects);
 
 export default router;
