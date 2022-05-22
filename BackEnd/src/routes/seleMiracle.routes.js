0b16b73a35c6170f5ec09d8cce0f3c23dfa2e148
LuisBola12 <luibv1202@gmail.com>
2022-05-21 19:24:22 -0600

    Modificaiones en el frontend, que permiten aparicion de casillas dependiendo del contrato e implementacion del post de un empleado

--- BackEnd/src/routes/seleMiracle.routes.js
+++ BackEnd/src/routes/seleMiracle.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getProjectsByEmail } from "../controllers/projects.controller";
 import { createNewUser, getUsers, getUserByEmail, getPeriodos } from "../controllers/users.controller";
-import { getEmployees } from '../controllers/employees.contoller'
+import { getEmployees, postNewEmployee} from '../controllers/employees.contoller'
 import { getVolDeductions, createNewVolDeduction } from "../controllers/volDeductions.controller";
 import { getBenefits } from "../controllers/benefits.controller";
+import { getTypeOfContracts } from "../controllers/contracts.controller";
 
 
 const router = Router();
@@ -13,7 +14,9 @@ router.get('/users/:Email', getUserByEmail);
 // router.get('/periodos',getPeriodos);
 // router.delete('/users',getUsers);
 // router.put('/users',getUsers);
-router.get('/employees', getEmployees);
+router.get('/typeContracts',getTypeOfContracts);
+router.get('/employees/:Proyecto', getEmployees);
+router.post('/employees',postNewEmployee);
 router.get('/projects/:Email', getProjectsByEmail);
 // router.post('/projects:User', createNewProjects);
 router.get('/benefits/:Proyecto', getBenefits);
