079d573cf2d365ad67ab43c9153169d87731ff51
Carlos <carlos.ace27@gmail.com>
2022-05-22 21:52:28 -0600

    Se implementa la parte del backEnd para insertar nuevos proyectos

--- BackEnd/src/controllers/projects.controller.js
+++ BackEnd/src/controllers/projects.controller.js
@@ -12,3 +12,26 @@ export const getProjectsByEmail = async (req, res) => {
     console.log(e);
   }
 };
+
+
+export const createProject = async (req, res) => {
+  const { Nombre, CedulaEmpleador, Periodo } = req.body;
+  if (Nombre == null || CedulaEmpleador == null || Periodo == null) {
+    const message = "Bad Request. Please Fill All Fields.";
+    return res.status(400).json({ msg: message });
+  }
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input("Nombre", Nombre)
+      .input("CedulaEmpleador", sql.VarChar, CedulaEmpleador)
+      .input("Periodo", sql.VarChar, Periodo)
+      .query(queries.createProject);
+    console.log(result);
+    res.json({ Nombre, CedulaEmpleador, Periodo });
+  } catch (e) {
+    console.log(`Error: ${e}`);
+    res.status(500).send(e.message);
+  }
+};
