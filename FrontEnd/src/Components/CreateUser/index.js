47378f0307357a07eb8a4e8b77286ad214ebd3cd
JAROD DANIEL VENEGAS ALPIZAR <jarod.venegas@ucr.ac.cr>
2022-05-27 18:18:09 -0600

    Cambios en el formulario de createUser

--- FrontEnd/src/Components/CreateUser/index.js
+++ FrontEnd/src/Components/CreateUser/index.js
@@ -1,10 +1,9 @@
-import { FormGroup } from "reactstrap";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { postLogin } from "../../Slices/user/requests/postLogin";
 import { usePostUserFromDatabase } from "./usePostUserFromDatabase";
 import { resetErrorMsg } from "../../Slices/user/userSlice";
-
+import { validateEmail, validatePassword } from "../../Validate";
 import './createUserStyle.scss';
 
 export const CreateUser = () => {
@@ -35,7 +34,7 @@ export const CreateUser = () => {
     setErrorMessage("");
   }
 
-  let navigate = useNavigate();
+  const navigate = useNavigate();
   function handleClick() {
     dispatch(resetErrorMsg());
     resetAllStates();
@@ -43,121 +42,125 @@ export const CreateUser = () => {
   }
 
   return (
-    <div className="register-page">
+    <div>
       <div className="register-bar">
-        <label className="register-logo"></label>
-        <label className="register-title">
-          Sign Up
-        </label>
+          <div className="register-logo"></div>
+          <div className="register-title"> Sign Up </div>
       </div>
+        
+      <form className="register-full-form">        
+        <div className="register-form-full-name">
+          <div className="register-animated-input-name">
+            <input 
+              type="text" 
+              id="name-employee" 
+              className="register-animated-input-name__input" 
+              value = {name} 
+              maxLength={15} 
+              onChange={(e) => setName(e.target.value)} 
+              autocomplete="off" 
+              placeholder=" "/>
+            <label for="name-employee" className="register-animated-input-name__label"> Name <span className="req">*</span></label>
+            <p className="errorForm" id="errorname"></p>
+          </div>
 
-      <div className="register-box">
-        <div className="register-full-name">
-          <FormGroup>
-              <div className="register-title-atribute">First Name</div>
-              <input
-                className="register-atribute-input-name"
-                maxLength="15"
-                type="text"
-                id="name"
-                placeholder="First Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />  
-          </FormGroup>
-          <FormGroup>
-          <div className="register-title-atribute">First Last Name</div>
-            <input
-              className="register-atribute-input-name"
-              maxLength="15"
-              type="text"
-              id="last-name1"
-              placeholder="First Last Name"
-              value={lastname1}
-              onChange={(e) => setLastName1(e.target.value)}
-            />
-          </FormGroup>
-          <FormGroup>
-          <div className="register-title-atribute">Second Last Name</div>
-            <input
-              className="register-atribute-input-name"
-              maxLength="15"
-              type="text"
-              id="last-name2"
-              placeholder="Second Last Name"
+          <div className="register-animated-input-name">
+            <input 
+              type="text" 
+              id="first-last-name-employee"
+              className="register-animated-input-name__input" 
+              value={lastname1} 
+              maxLength={15} 
+              onChange={(e) => setLastName1(e.target.value)} 
+              autocomplete="off" 
+              placeholder=" "/>
+            <label for="first-last-name-employee" className="register-animated-input-name__label">First Last Name<span className="req">*</span></label>
+            <p className="errorForm" id="error-first-lastname-input"></p>
+          </div>
+
+          <div className="register-animated-input-name">
+            <input 
+              type="text" 
+              id="second-last-name-employee" 
+              className="register-animated-input-name__input" 
               value={lastname2}
+              maxLength={15}
               onChange={(e) => setLastName2(e.target.value)}
-            />
-          </FormGroup>
+              autocomplete="off" 
+              placeholder=" "/>
+            <label for="second-last-name-employee" className="register-animated-input-name__label">Second Last Name<span className="req">*</span></label>
+            <p className="errorForm" id="error-second-lastname-input"></p>
+          </div>
+          
         </div>
-        <div className="register-id-phone">
-          <FormGroup>
-          <div className="register-title-atribute">ID Card</div>
-            <input
-              className="register-atribute-input-id"
-              maxLength="15"
-              type="text"
-              id="id-card"
-              placeholder="ID Card"
-              value={id}
-              onChange={(e) => setID(e.target.value)}
-            />
-          </FormGroup>
+        <div className="register-form-id-phone">
+          <div className="register-animated-input-id-phone">
+            <input 
+              type="text" 
+              id="id-employee" 
+              className="register-animated-input-id-phone__input" 
+              value={id} 
+              maxLength={15} 
+              onChange={(e) => setID(e.target.value)} 
+              autocomplete="off" 
+              placeholder=" "/>
+            <label for="id-employee" className="register-animated-input-id-phone__label">Id<span className="req">*</span></label>
+            <p className="errorForm" id="error-id-employee"></p>
+          </div>
+          <div className="register-animated-input-id-phone">
+            <input 
+              type="text" 
+              id="phone-number" 
+              className="register-animated-input-id-phone__input"
+              value={phoneNumber}
+              maxLength={8}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              autocomplete="off" 
+              placeholder=" "/>
+            <label for="phone-number" className="register-animated-input-id-phone__label">Phone Number<span className="req">*</span></label>
+          </div>
+        </div>
 
-          <FormGroup>
-          <input
-            className="register-atribute-input-id"
-            maxLength="8"
-            type="text"
-            id="phone-number"
-            placeholder=" "
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-           <label for="phone-number" className="register-label">Phone Number</label>
-        </FormGroup>
-        </div>
-        <div className="register-email-password">
-          <FormGroup>
-          <div className="register-title-atribute">Email</div>
-            <input
-              className="register-atribute-input-email"
-              maxLength="50"
-              type="text"
-              id="email"
-              placeholder="Email Address"
+        <div className="register-form-email-password">
+          <div className="register-animated-input-email">
+            <input 
+              type="text" 
+              id="email-employee" 
+              className="register-animated-input-email__input" 
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormGroup>
-          <FormGroup>
-          <div className="register-title-atribute">Password</div>
-            <input
-              className="register-atribute-input-email"
-              maxLength="20"
-              type="password"
-              id="password"
-              placeholder="Password"
+              maxLength={50}
+              onChange={(e) => { if(validateEmail(e.target.value)) { setEmail(e.target.value)} }}
+              autocomplete="off" 
+              placeholder=" "/>
+            <label for="email-employee" className="register-animated-input-email__label"> Email <span className="req">*</span> </label>
+            <p className="errorForm" id="error-email-input"></p>
+          </div>
+          <div className="register-animated-input-email">
+            <input 
+              type="password" 
+              id="password-employee" 
+              className="register-animated-input-email__input" 
               value={password}
+              maxLength={20}
               onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormGroup>
+              autocomplete="off" 
+              placeholder=" "/>
+            <p className="errorForm" id="error-password-input"></p>
+            <label for="password-employee" className="register-animated-input-email__label"> Password <span className="req">*</span> </label>
+          </div>
         </div>
-        <div className="register-submit-btn-box">
-          <button className="register-submit-btn" onClick={()=>{submitEmployee()}}>
-            Sign Up
+
+        <div className="register-btn-box">
+          <button className="register-btn-sumbit" >
+            Create
           </button>
-          <button className="register-cancel" onClick={handleClick}>
+          <button className="register-btn-cancel" >
             Cancel
           </button>
         </div>
-          {
-          errorMessage && (
-            <span className="register-errorMessage">{errorMessage}</span>
-          )
-          }
-      </div>
-      <footer className="register-footerCopyRights"> &copy; SeleMiracleRun </footer>
+
+      </form>
+      <footer className="logIn-footerCopyRights"> &copy; SeleMiracleRun </footer>
     </div>
   );
 };
