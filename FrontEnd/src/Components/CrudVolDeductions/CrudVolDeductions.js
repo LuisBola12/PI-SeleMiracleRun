02030f4056fb01795d2fd7b988c9716b352d2b73
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-06-03 11:37:40 -0600

    Se actualiza Vista de vol deductions

--- FrontEnd/src/Components/CrudVolDeductions/CrudVolDeductions.js
+++ FrontEnd/src/Components/CrudVolDeductions/CrudVolDeductions.js
@@ -3,6 +3,7 @@ import '../../App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useGetVolDeductionsFromDatabase } from "./useGetVolDeductionsFromDatabase";
 import { useNavigate } from "react-router-dom";
+import { transformCost } from "../../shared/moneyFormatTransform";
 
 export const CrudVolDeductions = () => {
   const navigate = useNavigate();
@@ -20,30 +21,31 @@ export const CrudVolDeductions = () => {
         <br />
         <table className="Table">
           <thead>
-            <tr className="table-header">
-              <th className="table-left-border">Voluntary Deduction </th>
-              <th>Cost</th>
-              <th>Description</th>
-              <th>Edit</th>
-              <th className="table-right-border">Delete</th>
+          <tr className="table-header">
+              <th className="table-left-border left-td">Voluntary Deduction</th>
+              <th className="left-td">Description</th>
+              <th className="right-td">Cost</th>
+              <th className="right-td">Edit</th>
+              <th className="table-right-border right-td">Delete</th>
             </tr>
           </thead>
           <tbody>
             {data.map((element) => (
               <tr key={element.Nombre}>
-                <td>{element.Nombre}</td>
-                <td>{element.Costo} ₡</td>
-                <td>{element.Descripcion} </td>
-                <td>
+                <td className="left-td">{element.Nombre}</td>
+                <td className="description-cell left-td">{((element.Descripcion) ? element.Descripcion : "No description")}</td>
+                <td className="right-td">{transformCost(element.Costo)}</td>
+                <td className="right-button">
                   <button className=" button"> Edit </button>
                 </td>
-                <td>
+                <td className="right-button">
                   <button className=" button cancel-button" > Delete </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+        <label className="Empty-volDeduction-message">{(data.length === 0) ? "No voluntary deductions added yet" : ""}</label>
       </Container>
     </>
   )
