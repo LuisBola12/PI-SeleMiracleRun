787773c02ff5b0de4b61cf903bf3b13cbb789993
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-06-03 11:38:07 -0600

    Se actualizan validaciones de vol Deductions

--- FrontEnd/src/Components/CrudVolDeductions/volDedutionValidations.js
+++ FrontEnd/src/Components/CrudVolDeductions/volDedutionValidations.js
@@ -1,12 +1,23 @@
 
 const validate = (values) => {
   let errors = {};
-  const regex = '';
   if (!values.Name) {
     errors.Name = 'Voluntary deduction Name Is Required';
+    document.getElementById('Name').style.borderColor = 'red';
+  } else {
+    document.getElementById('Name').style.borderColor = 'gray';
+  }
+  if (values.Name && values.Name[0] === '/') {
+    errors.Name = 'You must enter a valid name';
+    document.getElementById('Name').style.borderColor = 'red';
+  } else if (values.Name) {
+    document.getElementById('Name').style.borderColor = 'gray';
   }
   if (!values.Cost) {
     errors.Cost = 'Cost Required'
+    document.getElementById('Cost').style.borderColor = 'red';
+  } else {
+    document.getElementById('Cost').style.borderColor = 'gray';
   }
   return errors;
 };
