f49b12001bf366af7a4337499aebb3bd1b6bd810
LuisBola12 <luibv1202@gmail.com>
2022-06-06 13:10:28 -0600

    Commit con cambios en el css de la pagina de detalles de pago de empleados

--- FrontEnd/src/Components/PayrollDetails/PayrollDetails.js
+++ FrontEnd/src/Components/PayrollDetails/PayrollDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Container } from "reactstrap";
 import { IconContext } from "react-icons";
 import { FaArrowLeft } from 'react-icons/fa';
+import { transformCost } from "../../shared/moneyFormatTransform";
 
 export const PayrollDetails = () => {
     const data = [{
@@ -38,19 +38,23 @@ export const PayrollDetails = () => {
     <>
         <div className="table-button">
             <br />
-            <button className="button cancel-button">Back</button>
+                <IconContext.Provider value={{ color: "gray", className: "global-class-name", size: "3rem"}}>
+                <button className="back-arrow-button">
+                    <FaArrowLeft />
+                </button>
+            </IconContext.Provider>
           <br />
         </div>
         <table className="Table">
             <thead>
               <tr className="table-header">
-                <th className="table-left-border">Id</th>
-                <th className="">Name</th>
-                <th className="">Hours Worked</th>
-                <th className="">Gross Salary</th>
-                <th className="">Mandatory Deductions</th>
-                <th className="">Voluntary Deductions</th>
-                <th className="">Benefits</th>
+                <th className="left-td table-left-border">Id</th>
+                <th className="left-td">Name</th>
+                <th className="left-td">Hours Worked</th>
+                <th className="right-td">Gross Salary</th>
+                <th className="right-td">Mandatory Deductions</th>
+                <th className="right-td">Voluntary Deductions</th>
+                <th className="right-td">Benefits</th>
                 <th className="table-right-border">Net Salary</th>
                 {/* <th className="table-right-border">Employees Payslips</th> */}
               </tr>
@@ -58,14 +62,14 @@ export const PayrollDetails = () => {
             <tbody>
               {data.map((element) => (
                 <tr key={element.id}>
-                  <td className="">{element.id}</td>
-                  <td className="">{element.name}</td>
-                  <td className="">{element.hoursWorked}</td>
-                  <td className="">{element.grossSalary}</td>
-                  <td className="">{element.mandatoryDeductions}</td>
-                  <td className="">{element.voluntaryDeductions}</td>
-                  <td className="">{element.benefits}</td>
-                  <td className="">{element.netSalary}</td>
+                  <td className="left-td table-left-border">{element.id}</td>
+                  <td className="left-td">{element.name}</td>
+                  <td className="left-td">{element.hoursWorked}</td>
+                  <td className="right-td">₡{transformCost(element.grossSalary)}</td>
+                  <td className="right-td">₡{transformCost(element.mandatoryDeductions)}</td>
+                  <td className="right-td">₡{transformCost(element.voluntaryDeductions)}</td>
+                  <td className="right-td">₡{transformCost(element.benefits)}</td>
+                  <td className="table-right-border right-td">₡{transformCost(element.netSalary)}</td>
                 </tr>
               ))}
             </tbody>
