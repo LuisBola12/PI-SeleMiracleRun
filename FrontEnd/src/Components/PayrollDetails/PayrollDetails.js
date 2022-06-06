8020c451d57e6bee29979da10e95d22e05f1a218
LuisBola12 <luibv1202@gmail.com>
2022-06-06 16:10:38 -0600

    Commit con implementacion de columnas extra en la pantalla de pagos de empleados y con cambios en los estilos de la página

--- FrontEnd/src/Components/PayrollDetails/PayrollDetails.js
+++ FrontEnd/src/Components/PayrollDetails/PayrollDetails.js
@@ -14,32 +14,50 @@ export const PayrollDetails = () => {
     const data = [{
             'id':118020915,
             'name': 'Luis Bolanos',
+            'contract': 'Por Horas',
+            'hWage': 8000,
             'hoursWorked': 40,
-            'grossSalary': 750000,
-            'mandatoryDeductions':75000,
-            'voluntaryDeductions':40000,
+            'grossSalary': 320000,
+            'mandatoryDeductions':28800,
+            'voluntaryDeductions':23000,
             'benefits':35000,
-            'netSalary': 600000
+            'netSalary': 303200
         },{
-            'id':118020915,
-            'name': 'Luis Bolanos',
-            'hoursWorked': 40,
-            'grossSalary': 750000,
-            'mandatoryDeductions':75000,
-            'voluntaryDeductions':40000,
-            'benefits':35000,
-            'netSalary': 600000 
+            'id':308121271,
+            'name': 'Jarod Venegas',
+            'contract': 'Medio Tiempo',
+            'hWage': '-',
+            'hoursWorked': '-',
+            'grossSalary': 175000,
+            'mandatoryDeductions':15750,
+            'voluntaryDeductions':10000,
+            'benefits':15000,
+            'netSalary':  164250
         },
         {
-            'id':118020915,
-            'name': 'Luis Bolanos',
-            'hoursWorked': 40,
-            'grossSalary': 750000,
-            'mandatoryDeductions':75000,
+            'id':209120716,
+            'name': 'Javier Molina',
+            'contract': 'Tiempo Completo',
+            'hWage': '-',
+            'hoursWorked': '-',
+            'grossSalary': 550000,
+            'mandatoryDeductions':49500,
             'voluntaryDeductions':40000,
             'benefits':35000,
-            'netSalary': 600000
-        }
+            'netSalary': 495500
+        },
+        {
+          'id':401221716,
+          'name': 'Josef Ruzicka',
+          'contract': 'Servicios Profesionales',
+          'hWage': '-',
+          'hoursWorked': '-',
+          'grossSalary': '-',
+          'mandatoryDeductions': '-',
+          'voluntaryDeductions': '-',
+          'benefits': '-',
+          'netSalary': 350000
+      }
     ]
     const back = () => {
       history.push('/payroll')
@@ -51,9 +69,6 @@ export const PayrollDetails = () => {
     }, [])
   return (
     <>
-        <h1 className="details-head-title">
-          {`PaySlips from: ${startDate} - ${endDate}`}
-        </h1>
         <div className="details-table-button">
                 <IconContext.Provider value={{ color: "gray", className: "global-class-name", size: "3rem"}}>
                 <button className="back-arrow-button" onClick={()=>{back()}}>
@@ -62,17 +77,22 @@ export const PayrollDetails = () => {
               </IconContext.Provider>
             <br />
         </div>
+        <h1 className="details-head-title">
+          {`PaySlips from: ${startDate} - ${endDate}`}
+        </h1>
         <table className="Table">
             <thead>
               <tr className="table-header">
                 <th className="left-td table-left-border">Id</th>
                 <th className="left-td">Name</th>
-                <th className="left-td">Hours Worked</th>
+                <th className="left-td">Type of Contract</th>
+                <th className="right-td">Hours Worked</th>
+                <th className="right-td">Hourly Wage</th>
                 <th className="right-td">Gross Salary</th>
                 <th className="right-td">Mandatory Deductions</th>
                 <th className="right-td">Voluntary Deductions</th>
                 <th className="right-td">Benefits</th>
-                <th className="table-right-border">Net Salary</th>
+                <th className="table-right-border right-td">Net Salary</th>
                 {/* <th className="table-right-border">Employees Payslips</th> */}
               </tr>
             </thead>
@@ -81,12 +101,14 @@ export const PayrollDetails = () => {
                 <tr key={element.id}>
                   <td className="left-td table-left-border">{element.id}</td>
                   <td className="left-td">{element.name}</td>
-                  <td className="left-td">{element.hoursWorked}</td>
-                  <td className="right-td">₡{transformCost(element.grossSalary)}</td>
-                  <td className="right-td">₡{transformCost(element.mandatoryDeductions)}</td>
-                  <td className="right-td">₡{transformCost(element.voluntaryDeductions)}</td>
-                  <td className="right-td">₡{transformCost(element.benefits)}</td>
-                  <td className="table-right-border right-td">₡{transformCost(element.netSalary)}</td>
+                  <td className="left-td">{element.contract}</td>
+                  <td className="right-td">{element.hoursWorked}</td>
+                  <td className="right-td">{transformCost(element.hWage)}</td>
+                  <td className="right-td">{transformCost(element.grossSalary)}</td>
+                  <td className="right-td">{transformCost(element.mandatoryDeductions)}</td>
+                  <td className="right-td">{transformCost(element.voluntaryDeductions)}</td>
+                  <td className="right-td">{transformCost(element.benefits)}</td>
+                  <td className="table-right-border right-td">{transformCost(element.netSalary)}</td>
                 </tr>
               ))}
             </tbody>
