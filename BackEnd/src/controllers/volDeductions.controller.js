aa99fb39788fb7208ac1a8ad66cc92740595217a
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-06-05 23:25:09 -0600

    Se actualizan queries

--- BackEnd/src/controllers/volDeductions.controller.js
+++ BackEnd/src/controllers/volDeductions.controller.js
@@ -1,4 +1,4 @@
-import { getConnection,sql,queries } from "../database";
+import { getConnection,sql } from "../database";
 import { volDeductionsQuerys } from "../database/volDeductionsQuerys";
 
 export const getVolDeductions = async (req, res) => {
@@ -7,7 +7,7 @@ export const getVolDeductions = async (req, res) => {
     const pool = await getConnection();
     const result = await pool.request()
       .input('NombreProyecto', NombreProyecto)
-      .query(queries.getVolDeductions);
+      .query(volDeductionsQuerys.getVolDeductions);
     res.json(result.recordset);
   } catch (e) {
     res.status(500);
@@ -22,7 +22,7 @@ export const getVolDeductionsByName = async (req, res) => {
     const result = await pool.request()
       .input('Nombre', Nombre)
       .input('NombreProyecto', NombreProyecto)
-      .query(volDeductionsQueries.getVolDeductionsByName);
+      .query(volDeductionsQuerys.getVolDeductionsByName);
     res.json(result.recordset);
   } catch (e) {
     res.status(500);
@@ -44,9 +44,34 @@ export const createNewVolDeduction = async (req, res) => {
       .input("NombreProyecto", sql.VarChar, NombreProyecto)
       .input("Costo", sql.Int, Costo)
       .input("Descripcion", sql.VarChar, Descripcion)
-      .query(volDeductionsQueries.createNewVolDeduction);
+      .query(volDeductionsQuerys.createNewVolDeduction);
     console.log(result);
     res.json({ Nombre, Costo, Descripcion});
+  } catch (e) {
+    console.log(`Error: ${e}`);
+    res.status(500).send(e.message);
+  }
+};
+
+
+export const updateVolDeduction = async (req, res) => {
+  const { Nombre, NombreProyecto, Costo, Descripcion } = req.body;
+  const { NombreAntiguo } = req.params;
+  if (Nombre == null || Costo == null || NombreProyecto == null) {
+    const message = "Bad Request. Please Fill All Fields.";
+    return res.status(400).json({ msg: message });
+  }
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input("Nombre", sql.VarChar, Nombre)
+      .input("NombreAntiguo", sql.VarChar, NombreAntiguo)
+      .input("NombreProyecto", sql.VarChar, NombreProyecto)
+      .input("Costo", sql.Int, Costo)
+      .input("Descripcion", sql.VarChar, Descripcion)
+      .query(volDeductionsQuerys.editVolDeduction);
+    res.json({ Nombre, NombreProyecto, Costo, Descripcion })
   } catch (e) {
     console.log(`Error: ${e}`);
     res.status(500).send(e.message);
