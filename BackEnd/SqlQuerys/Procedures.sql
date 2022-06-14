a631bef41a5ffd136dab296ca61ba76f9c738427
JAROD DANIEL VENEGAS ALPIZAR <jarod.venegas@ucr.ac.cr>
2022-06-13 22:32:50 -0600

    Codigo sql de triggers y procedures

--- BackEnd/SqlQuerys/Procedures.sql
+++ BackEnd/SqlQuerys/Procedures.sql
@@ -16,3 +16,30 @@ BEGIN
 	Values (@CedulaEmpleado, @Proyecto, @CantidadHoras, @Fecha)
 
 END;
+
+Create Procedure DeleteAnEmployeeFromAProject @Cedula varchar(15), @Proyecto varchar(50)
+AS
+    Declare @NumeroDeBeneficios int;
+    Select @NumeroDeBeneficios = Count() from EmpleadoGozaBeneficios 
+    where CedulaEmpleado = @Cedula and NombreProyecto = @Proyecto;
+    If(@NumeroDeBeneficios > 0)
+    Begin
+        Delete from EmpleadoGozaBeneficios where CedulaEmpleado = @Cedula and NombreProyecto = @Proyecto;
+    End
+    Else
+        Begin
+            Print 'This Employee doesnt have benefits'
+        End
+
+    Declare @NumeroDeDeduccionesVoluntarias int;
+    Select @NumeroDeDeduccionesVoluntarias = Count() from EmpleadoAplicaDeduccionesVolundarias 
+    where CedulaEmpleado = @Cedula and NombreProyecto = @Proyecto;
+    If(@NumeroDeDeduccionesVoluntarias > 0)
+    Begin
+        Delete from EmpleadoAplicaDeduccionesVolundarias where CedulaEmpleado = @Cedula and NombreProyecto = @Proyecto;
+    End
+    Else
+        Begin
+            Print 'This Employee doesnt have Voluntary Deductions'
+        End
+    Delete from EmpleadoYContratoSeAsocianAProyecto where CedulaEmpleado = @Cedula and NombreProyecto = @Proyecto;
