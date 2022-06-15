b210325f163bb8c9e5f7fd94f0547b216748be70
Javier Molina Herrera <javmoli045@gmail.com>
2022-06-15 16:33:24 -0600

    Agregado al frontend la funcionalidad de desvincularse de un benecio siendo empleado

--- FrontEnd/src/Utils/Benefits/usePutToBenefits.js
+++ FrontEnd/src/Utils/Benefits/usePutToBenefits.js
@@ -2,6 +2,8 @@ import { useSelector } from 'react-redux';
 
 export const usePutToBenefits = () => {
   const activeProject = useSelector((state) => state.activeProject.projectName);
+  const employeeEmail = useSelector((state) => state.user.user.Email);
+  const unlinkBenefitApi = 'http://localhost:4000/myBenefits'
 
   const updateBenefit = async (name, cost, description, apiBenefits) => {
     const newCost = cost.split('.').join('');
@@ -19,7 +21,23 @@ export const usePutToBenefits = () => {
     });
     console.log(postFetch);
   };
+
+  const unlinkEmployeeToBenefit = async (name) => {
+    const postFetch = await fetch(unlinkBenefitApi, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        Email: employeeEmail,
+        Proyecto: activeProject,
+        NombreBeneficio: name
+      }),
+    });
+    console.log(postFetch);
+  };
+
   return {
-    updateBenefit
+    updateBenefit, unlinkEmployeeToBenefit
   };
 };
