500ae3d10e2a18e65a4ecf1ebc85e65cf2a50896
Carlos <carlos.ace27@gmail.com>
2022-06-18 09:02:58 -0600

    Se crea el query benefitsUsedAndLimits para posteriormente validar una suscripcion a beneficio por parte de un empleado

--- BackEnd/src/database/queries/benefitsQueries.js
+++ BackEnd/src/database/queries/benefitsQueries.js
@@ -1,7 +1,33 @@
 export const benefitsQueries = {
-  getBenefits: `Select * from Beneficios b where NombreProyecto = @Proyecto and b.Activo = 'true'`,
+  getBenefits: 'Select * from Beneficios b where NombreProyecto = @Proyecto and b.Activo = \'true\'',
   getBenefitsByName: 'select Nombre from Beneficios where Nombre = @Nombre and NombreProyecto = @Proyecto',
-  createBenefit: `Insert into Beneficios (Nombre, NombreProyecto, CostoActual, Descripción, Activo) values (@Nombre, @NombreProyecto ,@CostoActual, @Descripción, 'true')`,
-  editBenefit: `Update Beneficios set Nombre = @Nombre, CostoActual = @CostoActual, Descripción = @Descripción where NombreProyecto=@NombreProyecto and Nombre=@NombreAntiguo and Activo = 'true'`,
+  createBenefit: 'Insert into Beneficios (Nombre, NombreProyecto, CostoActual, Descripción, Activo) values (@Nombre, @NombreProyecto ,@CostoActual, @Descripción, \'true\')',
+  editBenefit: 'Update Beneficios set Nombre = @Nombre, CostoActual = @CostoActual, Descripción = @Descripción where NombreProyecto=@NombreProyecto and Nombre=@NombreAntiguo and Activo = \'true\'',
+  benefitsUsedAndLimits:
+  `
+SELECT  Empleado.Nombre as EmployeeName, Empleado.Apellido1 as EmployeeLastName
+,BeneficioElegido.NombreProyecto as ProjectName
+,count(BeneficioElegido.NombreBeneficio)  as EmployeeBenefitsQty
+, sum(Beneficios.CostoActual) as MoneyAmountUsedByEmployee
+
+FROM Usuarios
+JOIN Empleado ON Usuarios.Email = Empleado.Email
+JOIN EmpleadoYContratoSeAsocianAProyecto e 
+ON Empleado.Cedula = e.CedulaEmpleado
+JOIN BeneficioElegido ON BeneficioElegido.CedulaEmpleado = Empleado.Cedula
+JOIN Beneficios ON Beneficios.Nombre = BeneficioElegido.NombreBeneficio
+
+WHERE Empleado.Email = @employeeEmail
+AND BeneficioElegido.fechaFin >  GETDATE()
+AND BeneficioElegido.NombreProyecto = @projectName
+GROUP BY Empleado.Nombre, Empleado.Apellido1,BeneficioElegido.NombreProyecto
+GO 
+
+SELECT Proyecto.Nombre as ProjectName
+,CantidadMaximaBeneficiosEmpleado as maxBenefitsQtyAllowed
+, MontoMaximoBeneficiosEmpleado as maxMoneyAmountAllowed 
+FROM Proyecto
+WHERE Proyecto.Nombre = @ProjectName 
+  `
 };
 
