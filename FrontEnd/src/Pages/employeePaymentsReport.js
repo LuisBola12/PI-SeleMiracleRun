6ae56d7a5dbac087ddbc86d2ef9fce3c238d1701
Javier Molina Herrera <javmoli045@gmail.com>
2022-07-04 19:47:17 -0600

    Agregado un bóton en el drop down menu para acceder a mis reportes de pago

--- FrontEnd/src/Pages/employeePaymentsReport.js
+++ FrontEnd/src/Pages/employeePaymentsReport.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../App.css';
-import { CrudBenefits } from '../Components/CrudBenefits/CrudBenefits';
 import { Navbar } from '../Components/Navbar/Navbar';
 
-export const Benefits = () => {
+export const EmployeePaymentsReport = () => {
   return (
     <>
       <div className='sticky-navbar'>
         <Navbar />
       </div>
       <div className='page-content'>
-        {<CrudBenefits />}
+
       </div>
       <footer>&copy; Sele Miracle Run - UCR</footer>
     </>
