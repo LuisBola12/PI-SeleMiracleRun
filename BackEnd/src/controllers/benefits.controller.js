fd13474877f0365c21a34c1bf816185f02556e58
Javier Molina Herrera <javmoli045@gmail.com>
2022-07-12 16:32:23 -0600

    Ahora al elminar un beneficio se le agrega un * en la base de datos, esto se usará en la funcionalidad de reactivar beneficios

--- BackEnd/src/controllers/benefits.controller.js
+++ BackEnd/src/controllers/benefits.controller.js
@@ -3,208 +3,209 @@ import { benefitsQueries } from '../database/queries/benefitsQueries';
 import { getConsecutivePayNumber } from './payrollController';
 import { notifyEmployeesForDeletedBenefit } from '../utils/notifyEmployeesForDeletedBenefit';
 
-export const getBenefits = async (req, res) => {
+export const getBenefits = async ( req, res ) => {
   const { Proyecto, CedulaEmpleador } = req.params;
   try {
     const pool = await getConnection();
     const result = await pool.request()
-      .input('Proyecto', Proyecto)
-      .input('CedulaEmpleador', CedulaEmpleador)
-      .query(benefitsQueries.getBenefits);
-    res.json(result.recordset);
-  } catch (e) {
-    res.status(500);
-    res.send(e.message);
+      .input( 'Proyecto', Proyecto )
+      .input( 'CedulaEmpleador', CedulaEmpleador )
+      .query( benefitsQueries.getBenefits );
+    res.json( result.recordset );
+  } catch ( e ) {
+    res.status( 500 );
+    res.send( e.message );
   }
 };
 
-export const getBenefitsByName = async (req, res) => {
+export const getBenefitsByName = async ( req, res ) => {
   const { Proyecto, CedulaEmpleador, Nombre } = req.params;
   try {
     const pool = await getConnection();
     const result = await pool.request()
-      .input('Nombre', Nombre)
-      .input('Proyecto', Proyecto)
-      .input('CedulaEmpleador', CedulaEmpleador)
-      .query(benefitsQueries.getBenefitsByName);
-    res.json(result.recordset);
-    console.log(result.recordset);
-  } catch (e) {
-    res.status(500);
-    res.send(e.message);
+      .input( 'Nombre', Nombre )
+      .input( 'Proyecto', Proyecto )
+      .input( 'CedulaEmpleador', CedulaEmpleador )
+      .query( benefitsQueries.getBenefitsByName );
+    res.json( result.recordset );
+    console.log( result.recordset );
+  } catch ( e ) {
+    res.status( 500 );
+    res.send( e.message );
   }
 };
 
-export const getEmployeeBenefitsByEmail = async (req, res) => {
+export const getEmployeeBenefitsByEmail = async ( req, res ) => {
   const { Proyecto, Email } = req.params;
   try {
     const pool = await getConnection();
     const result = await pool.request()
-      .input('Email', Email)
-      .input('Proyecto', Proyecto)
-      .execute('getEmployeeBenefits');
-    res.json(result.recordset);
-    console.log(result.recordset);
-  } catch (e) {
-    res.status(500);
-    res.send(e.message);
+      .input( 'Email', Email )
+      .input( 'Proyecto', Proyecto )
+      .execute( 'getEmployeeBenefits' );
+    res.json( result.recordset );
+    console.log( result.recordset );
+  } catch ( e ) {
+    res.status( 500 );
+    res.send( e.message );
   }
 };
 
 
-export const getOfferedBenefits = async (req, res) => {
+export const getOfferedBenefits = async ( req, res ) => {
   const { Proyecto, Email } = req.params;
   try {
     const pool = await getConnection();
     const result = await pool.request()
-      .input('Email', Email)
-      .input('Proyecto', Proyecto)
-      .execute('getOfferedBenefits');
-    res.json(result.recordset);
-    console.log(result.recordset);
-  } catch (e) {
-    res.status(500);
-    res.send(e.message);
+      .input( 'Email', Email )
+      .input( 'Proyecto', Proyecto )
+      .execute( 'getOfferedBenefits' );
+    res.json( result.recordset );
+    console.log( result.recordset );
+  } catch ( e ) {
+    res.status( 500 );
+    res.send( e.message );
   }
 };
 
 
-export const linkEmployeeToBenefit = async (req, res) => {
+export const linkEmployeeToBenefit = async ( req, res ) => {
   const { Email, NombreBeneficio, NombreProyecto } = req.body;
   try {
     const pool = await getConnection();
     const result = await pool
       .request()
-      .input('Email', sql.VarChar, Email)
-      .input('NombreProyecto', sql.VarChar, NombreProyecto)
-      .input('NombreBeneficio', sql.VarChar, NombreBeneficio)
-      .execute('vincularBeneficioEmpleado');
-    console.log(result);
-    res.json({ NombreProyecto, NombreBeneficio });
-  } catch (e) {
-    console.log(`Error: ${e}`);
-    res.status(500).send(e.message);
+      .input( 'Email', sql.VarChar, Email )
+      .input( 'NombreProyecto', sql.VarChar, NombreProyecto )
+      .input( 'NombreBeneficio', sql.VarChar, NombreBeneficio )
+      .execute( 'vincularBeneficioEmpleado' );
+    console.log( result );
+    res.json( { NombreProyecto, NombreBeneficio } );
+  } catch ( e ) {
+    console.log( `Error: ${e}` );
+    res.status( 500 ).send( e.message );
   }
 };
 
-export const unlinkEmployeeToBenefit = async (req, res) => {
+export const unlinkEmployeeToBenefit = async ( req, res ) => {
   const { Email, Proyecto, NombreBeneficio } = req.body;
   try {
     const pool = await getConnection();
     const result = await pool
       .request()
-      .input('Email', sql.VarChar, Email)
-      .input('Proyecto', sql.VarChar, Proyecto)
-      .input('NombreBeneficio', sql.VarChar, NombreBeneficio)
-      .execute('desvincularBeneficioDeEmpleado');
-    console.log(result);
-  } catch (e) {
-    console.log(`Error: ${e}`);
-    res.status(500).send(e.message);
+      .input( 'Email', sql.VarChar, Email )
+      .input( 'Proyecto', sql.VarChar, Proyecto )
+      .input( 'NombreBeneficio', sql.VarChar, NombreBeneficio )
+      .execute( 'desvincularBeneficioDeEmpleado' );
+    console.log( result );
+  } catch ( e ) {
+    console.log( `Error: ${e}` );
+    res.status( 500 ).send( e.message );
   }
 };
 
 
-export const createBenefit = async (req, res) => {
+export const createBenefit = async ( req, res ) => {
   const { Nombre, NombreProyecto, CedulaEmpleador, CostoActual, Descripción } = req.body;
-  if (Nombre == null || CostoActual == null || NombreProyecto == null) {
+  if ( Nombre == null || CostoActual == null || NombreProyecto == null ) {
     const message = 'Bad Request. Please Fill All Fields.';
-    return res.status(400).json({ msg: message });
+    return res.status( 400 ).json( { msg: message } );
   }
   try {
     const pool = await getConnection();
     const result = await pool
       .request()
-      .input('Nombre', sql.VarChar, Nombre)
-      .input('NombreProyecto', sql.VarChar, NombreProyecto)
-      .input('CedulaEmpleador', sql.VarChar, CedulaEmpleador)
-      .input('CostoActual', sql.Int, CostoActual)
-      .input('Descripción', sql.VarChar, Descripción)
-      .query(benefitsQueries.createBenefit);
-    console.log(result);
-    res.json({ Nombre, CostoActual });
-  } catch (e) {
-    console.log(`Error: ${e}`);
-    res.status(500).send(e.message);
+      .input( 'Nombre', sql.VarChar, Nombre )
+      .input( 'NombreProyecto', sql.VarChar, NombreProyecto )
+      .input( 'CedulaEmpleador', sql.VarChar, CedulaEmpleador )
+      .input( 'CostoActual', sql.Int, CostoActual )
+      .input( 'Descripción', sql.VarChar, Descripción )
+      .query( benefitsQueries.createBenefit );
+    console.log( result );
+    res.json( { Nombre, CostoActual } );
+  } catch ( e ) {
+    console.log( `Error: ${e}` );
+    res.status( 500 ).send( e.message );
   }
 };
 
-export const updateBenefit = async (req, res) => {
+export const updateBenefit = async ( req, res ) => {
   const { Nombre, NombreProyecto, CedulaEmpleador, CostoActual, Descripción } = req.body;
   const { NombreAntiguo } = req.params;
-  if (Nombre == null || CostoActual == null || NombreProyecto == null) {
+  if ( Nombre == null || CostoActual == null || NombreProyecto == null ) {
     const message = 'Bad Request. Please Fill All Fields.';
-    return res.status(400).json({ msg: message });
+    return res.status( 400 ).json( { msg: message } );
   }
   try {
     const pool = await getConnection();
     const result = await pool
       .request()
-      .input('Nombre', sql.VarChar, Nombre)
-      .input('NombreAntiguo', sql.VarChar, NombreAntiguo)
-      .input('NombreProyecto', sql.VarChar, NombreProyecto)
-      .input('CedulaEmpleador', sql.VarChar, CedulaEmpleador)
-      .input('CostoActual', sql.Int, CostoActual)
-      .input('Descripción', sql.VarChar, Descripción)
-      .query(benefitsQueries.editBenefit);
-    res.json({ Nombre, NombreProyecto, CostoActual, Descripción });
-  } catch (e) {
-    console.log(`Error: ${e}`);
-    res.status(500).send(e.message);
+      .input( 'Nombre', sql.VarChar, Nombre )
+      .input( 'NombreAntiguo', sql.VarChar, NombreAntiguo )
+      .input( 'NombreProyecto', sql.VarChar, NombreProyecto )
+      .input( 'CedulaEmpleador', sql.VarChar, CedulaEmpleador )
+      .input( 'CostoActual', sql.Int, CostoActual )
+      .input( 'Descripción', sql.VarChar, Descripción )
+      .query( benefitsQueries.editBenefit );
+    res.json( { Nombre, NombreProyecto, CostoActual, Descripción } );
+  } catch ( e ) {
+    console.log( `Error: ${e}` );
+    res.status( 500 ).send( e.message );
   }
 };
 
-export const insertCostTotalBenefits = async (cedEmpleado, proyName, consecutivePayroll) => {
-  const consecutivePayslip = await getConsecutivePayNumber(consecutivePayroll, cedEmpleado);
+export const insertCostTotalBenefits = async ( cedEmpleado, proyName, consecutivePayroll ) => {
+  const consecutivePayslip = await getConsecutivePayNumber( consecutivePayroll, cedEmpleado );
   try {
     const pool = await getConnection();
     const result = await pool.request()
-      .input('CedulaEmpleado', cedEmpleado)
-      .input('Proyecto', proyName)
-      .input('ConsecutivoPlanilla', consecutivePayroll)
-      .input('ConsecutivoPago', consecutivePayslip)
-      .execute('insertarBeneficiosEnPago');
+      .input( 'CedulaEmpleado', cedEmpleado )
+      .input( 'Proyecto', proyName )
+      .input( 'ConsecutivoPlanilla', consecutivePayroll )
+      .input( 'ConsecutivoPago', consecutivePayslip )
+      .execute( 'insertarBeneficiosEnPago' );
     return true;
-  } catch (e) {
-    console.log(e);
+  } catch ( e ) {
+    console.log( e );
     return undefined;
   }
 };
-export const deactivateBenefit = async (req, res) => {
-  const { Nombre, NombreProyecto } = req.body;
+export const deactivateBenefit = async ( req, res ) => {
+  const { Nombre, NombreProyecto, CedulaEmpleador } = req.body;
   try {
     const pool = await getConnection();
     const result = await pool
       .request()
-      .input('NombreBeneficio', sql.VarChar, Nombre)
-      .input('Proyecto', sql.VarChar, NombreProyecto)
-      .execute('eliminarBeneficio');
-    await notifyEmployeesForDeletedBenefit(result.recordset, Nombre, NombreProyecto);
-  } catch (e) {
-    console.log(`Error: ${e}`);
-    res.status(500).send(e.message);
+      .input( 'NombreBeneficio', sql.VarChar, Nombre )
+      .input( 'Proyecto', sql.VarChar, NombreProyecto )
+      .input( 'CedulaEmpleador', sql.VarChar, CedulaEmpleador )
+      .execute( 'eliminarBeneficio' );
+    await notifyEmployeesForDeletedBenefit( result.recordset, Nombre, NombreProyecto );
+  } catch ( e ) {
+    console.log( `Error: ${e}` );
+    res.status( 500 ).send( e.message );
   }
 };
 
-export const CostTotalBenefits = async (infoBenefits) => {
+export const CostTotalBenefits = async ( infoBenefits ) => {
   const { Email, Proyecto, ConsecutivoPlanilla, ConsecutivoPago } = infoBenefits.body;
   try {
     const pool = await getConnection();
     const result = await pool.request()
-      .input('Email', Email)
-      .input('Proyecto', Proyecto)
-      .input('ConsecutivoPlanilla', ConsecutivoPlanilla)
-      .input('ConsecutivoPago', ConsecutivoPago)
-      .execute('calcularTotalBeneficiosDeEmpleado');
-    console.log(result.recordset);
+      .input( 'Email', Email )
+      .input( 'Proyecto', Proyecto )
+      .input( 'ConsecutivoPlanilla', ConsecutivoPlanilla )
+      .input( 'ConsecutivoPago', ConsecutivoPago )
+      .execute( 'calcularTotalBeneficiosDeEmpleado' );
+    console.log( result.recordset );
     return result.recordset;
-  } catch (e) {
-    console.log(e);
+  } catch ( e ) {
+    console.log( e );
     return undefined;
   }
 };
 
-export const validateBenefitSuscription = async (req, res) => {
+export const validateBenefitSuscription = async ( req, res ) => {
   const { projectName, employeeEmail, benefitToValidate } = req.params;
 
 
@@ -220,22 +221,22 @@ export const validateBenefitSuscription = async (req, res) => {
 
     const pool = await getConnection();
     const benefitToValidateInfo = await pool.request()
-      .input('benefitName', benefitToValidate)
-      .input('projectName', projectName)
-      .query(benefitsQueries.getBenefitInfo);
+      .input( 'benefitName', benefitToValidate )
+      .input( 'projectName', projectName )
+      .query( benefitsQueries.getBenefitInfo );
 
     const benefitsUsedInfo = await pool.request()
-      .input('employeeEmail', employeeEmail)
-      .input('projectName', projectName)
-      .query(benefitsQueries.benefitUsedInfo);
+      .input( 'employeeEmail', employeeEmail )
+      .input( 'projectName', projectName )
+      .query( benefitsQueries.benefitUsedInfo );
 
     const benefitsLimits = await pool.request()
-      .input('projectName', projectName)
-      .query(benefitsQueries.benefitsLimits);
+      .input( 'projectName', projectName )
+      .query( benefitsQueries.benefitsLimits );
     let employeeBenefitsQty = 0;
     let moneyAmountUsedByEmployee = 0;
 
-    if (await benefitsUsedInfo.recordset.length > 0) {
+    if ( await benefitsUsedInfo.recordset.length > 0 ) {
       const { employeeBenefitsQty: extractedQty, moneyAmountUsedByEmployee: extractedMoney } = await benefitsUsedInfo.recordset[0];
       employeeBenefitsQty = extractedQty;
       moneyAmountUsedByEmployee = extractedMoney;
@@ -246,21 +247,21 @@ export const validateBenefitSuscription = async (req, res) => {
 
     validation.maxBenefitsQtyAllowed = maxBenefitsQtyAllowed;
     validation.maxMoneyAmountAllowed = maxMoneyAmountAllowed;
-    if (employeeBenefitsQty + 1 > maxBenefitsQtyAllowed) {
+    if ( employeeBenefitsQty + 1 > maxBenefitsQtyAllowed ) {
       validation.isValid = false;
       validation.exceedsBenefitsQtyLimit = true;
     }
-    if (moneyAmountUsedByEmployee + benefitToValidateCost > maxMoneyAmountAllowed) {
+    if ( moneyAmountUsedByEmployee + benefitToValidateCost > maxMoneyAmountAllowed ) {
       validation.isValid = false;
       validation.exceedsMoneyAmountLimit = true;
     }
 
-    res.json(validation);
+    res.json( validation );
 
-    console.log(validation);
+    console.log( validation );
   }
-  catch (error) {
-    res.status(500).send('Database Conection Error:' + error.message);
-    console.log(error);
+  catch ( error ) {
+    res.status( 500 ).send( 'Database Conection Error:' + error.message );
+    console.log( error );
   }
 };
