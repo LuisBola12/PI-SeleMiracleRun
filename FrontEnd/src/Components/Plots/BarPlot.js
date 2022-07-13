a598ed009e2d9097341276c1d03217a6aa49bad1
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-07-13 12:05:46 -0600

    Avance de grafico de barras generico

--- FrontEnd/src/Components/Plots/BarPlot.js
+++ FrontEnd/src/Components/Plots/BarPlot.js
@@ -1,51 +1,52 @@
 import React from 'react';
-import {Bar} from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-export const BarPlot = ({dataLabels, PlotLabel, dataValues}) => {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-  const state = {
-    // TODO: Traer beneficios desde backend.
-    labels: ['Fiesta de pizza', 'Dentista', 'Entradas para cine'],
+export const BarPlot = ({plotLabel ,dataLabels, dataValues}) => {
+  
+  console.log(plotLabel ,dataLabels, dataValues)
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: plotLabel,
+      },
+    },
+  };
+  
+  const labels = dataLabels;
+  const data = {
+    labels,
     datasets: [
       {
-        // TODO: cambiar por label
-        label: 'Employees by benefit',
-        backgroundColor: 'rgba(75,192,192,1)',
-        borderColor: 'rgba(0,0,0,1)',
-        borderWidth: 2,
-        // TODO: Traer cantidad desde backend
-        data: [4, 6, 2],
-      }
-    ]
-  }
-
-  return (
-    <div>
-      <Bar
-        data={state}
-        options={{
-          title:{
-            display:true,
-            // TODO: Cambiar por label
-            text:'Benefits chosen by employees',
-            fontSize:20
-          },
-          legend:{
-            display:true,
-            position:'right'
-          }, 
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true,
-                min: 0,
-                max: 10,
-                stepSize: 1,
-              }
-            }]
-          }
-        }}
-      />
-    </div>
-  );
+        data: dataValues,
+        backgroundColor: ['rgba(255, 99, 132, 0.5)', 
+                          'rgba(155, 255, 132, 0.5)', 
+                          'rgba(155, 120, 255, 0.5)', 
+                          'rgba(255, 255, 255, 0.5)'],
+      },
+    ],
+  };
+  return <Bar options={options} data={data} />;
 }
