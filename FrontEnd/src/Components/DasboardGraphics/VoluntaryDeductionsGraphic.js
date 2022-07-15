456e7642f38956eee6ca640e41ea6cb7e258b3ef
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-07-15 16:26:33 -0600

    Funcionalidad frontend de grafico de voluntary deductions

--- FrontEnd/src/Components/DasboardGraphics/VoluntaryDeductionsGraphic.js
+++ FrontEnd/src/Components/DasboardGraphics/VoluntaryDeductionsGraphic.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import { React, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { BarPlot } from '../Plots/BarPlot'
+import { getVoluntaryDeductionsStatistics } from '../../Utils/VoluntaryDeductions/getVoluntaryDeductionsStatistics';
 
 export const VoluntaryDeductionsGraphic = () => {
 
-  const voluntaryDeductionsLables = ['Gym', 'Almuerzo', 'Transporte'];
-  const voluntaryDeductionsData = [4,3,7];
+  const user = useSelector((state) => state.user.user);
+  const [voluntaryDeductionsLables, setVoluntaryDeductionsLabels] = useState([]);
+  const [voluntaryDeductionsData, setVoluntaryDeductionsData] = useState([]);
+  useEffect(() => {
+    console.log('useEffect');
+    const getStatistics = async () => {
+      const data = await getVoluntaryDeductionsStatistics(user.Cedula);
+      console.log(data);
+      if (data) {
+        let labels = [];
+        let dataValues = [];
+        data.forEach((element) => {
+          labels.push(element.Nombre);
+          dataValues.push(element.empleados);
+        });
+        setVoluntaryDeductionsLabels(labels);
+        setVoluntaryDeductionsData(dataValues);
+        console.log(labels);
+        console.log(dataValues);
+      }
+    };
+    getStatistics();
+  }, []);
+  //const voluntaryDeductionsLables = ['Gym', 'Almuerzo', 'Transporte'];
+  //const voluntaryDeductionsData = [4, 3, 7];
 
 
   return (
@@ -12,10 +37,10 @@ export const VoluntaryDeductionsGraphic = () => {
       <h3> Voluntary deductions selected by employees</h3>
       <BarPlot
         dataLabels={voluntaryDeductionsLables}
-        dataValues= {voluntaryDeductionsData}
+        dataValues={voluntaryDeductionsData}
         plotLabel={'Voluntary Deductions'}
       />
-    </div>    
+    </div>
 
   )
 }
